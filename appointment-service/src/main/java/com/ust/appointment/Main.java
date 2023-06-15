db720b490391b9434f3e73e77424030f85ed1cda
sayan.binujacob <sayan.binujacob@ust.com>
2023-06-15 14:42:20 +0530

    exceptions

--- appointment-service/src/main/java/com/ust/appointment/Main.java
+++ appointment-service/src/main/java/com/ust/appointment/Main.java
@@ -2,11 +2,13 @@ package com.ust.appointment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 
 public class Main {
     public static void main(String[] args) {
--- gateway-service/src/main/java/com/ust/gateway/Main.java
+++ gateway-service/src/main/java/com/ust/gateway/Main.java
@@ -1,4 +1,4 @@
-package com.ust.admin;
+package com.ust.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
